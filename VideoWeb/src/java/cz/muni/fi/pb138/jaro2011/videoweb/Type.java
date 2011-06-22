e74fc72a32966300da2b32a6e3d1487b1f574bed
traninho90@gmail.com <traninho90@gmail.com@c4821f4a-e21e-2dad-b0b3-6af572229c24>
2011-06-22 09:57:29 +0000

    banzaaai

--- VideoWeb/src/java/cz/muni/fi/pb138/jaro2011/videoweb/Type.java
+++ VideoWeb/src/java/cz/muni/fi/pb138/jaro2011/videoweb/Type.java
@@ -5,5 +5,33 @@ package cz.muni.fi.pb138.jaro2011.videoweb;
  * @author Honza
  */
 public enum Type {
-    ORIGINAL, MAGAZINE, HOME, COPY
+    ORIGINAL
+    {
+        public String toSTring()
+        {
+            return "originál";
+        }
+    }, 
+    MAGAZINE
+    {
+        public String toString()
+        {
+            return "magazín";
+        }
+    }, 
+    HOME
+    {
+        public String toString()
+        {
+            return "domácí";
+        }
+    }, 
+    COPY
+    {
+        public String toString()
+        {
+            return "kopie";
+        }
+    }
 }
+
