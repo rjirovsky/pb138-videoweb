49a241c21dcf821fc34f3f856aa7e79adb56371c
rjirovsky@gmail.com <rjirovsky@gmail.com@c4821f4a-e21e-2dad-b0b3-6af572229c24>
2011-06-22 10:28:39 +0000

    manager get methods, lib directory

--- trunk/VideoWeb/src/java/cz/muni/fi/pb138/jaro2011/videoweb/DvdManagerImpl.java
+++ trunk/VideoWeb/src/java/cz/muni/fi/pb138/jaro2011/videoweb/DvdManagerImpl.java
@@ -1,13 +1,26 @@
 package cz.muni.fi.pb138.jaro2011.videoweb;
 
-import java.io.OutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.xml.transform.OutputKeys;
+import org.exist.xmldb.XQueryService;
+import org.w3c.dom.Document;
+import org.w3c.dom.DocumentFragment;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 
 import org.xmldb.api.DatabaseManager;
 import org.xmldb.api.base.Collection;
+import org.xmldb.api.base.CompiledExpression;
 import org.xmldb.api.base.Database;
+import org.xmldb.api.base.Resource;
+import org.xmldb.api.base.ResourceIterator;
+
+import org.xmldb.api.base.ResourceSet;
 import org.xmldb.api.base.XMLDBException;
 import org.xmldb.api.modules.XMLResource;
+import org.xmldb.api.modules.XPathQueryService;
 
 /**
  *
@@ -15,70 +28,49 @@ import org.xmldb.api.modules.XMLResource;
  */
 public class DvdManagerImpl implements DvdManager {
 
-    private static String URI = "xmldb:exist://localhost:8080/exist/xmlrpc/db";
+    private static String dbURI = "xmldb:exist://localhost:8080/exist/xmlrpc/db";
 
     public DvdManagerImpl() throws ClassNotFoundException, InstantiationException, IllegalAccessException, XMLDBException {
 
         final String driver = "org.exist.xmldb.DatabaseImpl";
-// initialize database driver
 
+        // initialize database driver
         Class cl = Class.forName(driver);
-
         Database database = (Database) cl.newInstance();
-
-        database.setProperty("create-database", "true");
-
+        //database.setProperty("create-database", "true");
         DatabaseManager.registerDatabase(database);
+    }
 
-
-
+    @Override
+    public void createDvd(Dvd dvd) {
         Collection col = null;
-
-        XMLResource res = null;
 
         try {
 
-// get the collection
-
-            col = DatabaseManager.getCollection(URI);
-
-            col.setProperty(OutputKeys.INDENT, "no");
-
-            res = (XMLResource) col.getResource("dvd.xml");
+            col = DatabaseManager.getCollection(dbURI);
+            XQueryService xqs = (XQueryService) col.getService("XQueryService", "1.0");
+            xqs.setProperty("indent", "yes");
 
 
 
-            if (res == null) {
+            CompiledExpression compiled = xqs.compile("xquery");
 
-                System.out.println("document not found!");
+            ResourceSet result = xqs.execute(compiled);
 
-            } else {
 
-                System.out.println(res.getContent());
-
-            }
-
+        } catch (XMLDBException ex) {
+            Logger.getLogger(DvdManagerImpl.class.getName()).log(Level.SEVERE, null, ex);
         } finally {
 //dont forget to cleanup
-
             if (col != null) {
-
                 try {
                     col.close();
                 } catch (XMLDBException xe) {
                     xe.printStackTrace();
                 }
-
             }
-
-
         }
 
-    }
-
-    @Override
-    public void createDvd(Dvd dvd) {
-        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
@@ -97,22 +89,147 @@ public class DvdManagerImpl implements DvdManager {
     }
 
     @Override
-    public OutputStream getDvdById(long id) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public Document getDvdById(long id) {
+        Document dvd = null;
+        Collection col = null;
+        try {
+            col = DatabaseManager.getCollection(dbURI);
+            XPathQueryService xpqs = (XPathQueryService) col.getService("XPathQueryService", "1.0");
+            xpqs.setProperty("indent", "yes");
+            ResourceSet result = xpqs.query("//dvd[@id = " + id + "]");
+
+            XMLResource xmlRes = (XMLResource) result.getMembersAsResource();
+
+            dvd = (Document) xmlRes.getContentAsDOM();
+
+        } catch (XMLDBException ex) {
+            Logger.getLogger(DvdManagerImpl.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (col != null) {
+                try {
+                    col.close();
+                } catch (XMLDBException xe) {
+                    System.err.println("Chyba při získávání dat z DB! " + xe);
+                }
+            }
+            return dvd;
+        }
+
     }
 
     @Override
-    public OutputStream getAllDvds() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public Document getAllDvds() {
+        Collection col = null;
+        Document allDvds = null;
+
+        try {
+            col = DatabaseManager.getCollection(dbURI);
+            col.setProperty(OutputKeys.INDENT, "no");
+            XMLResource xmlRes = (XMLResource) col.getResource("dvd.xml");      // ziska cely xml dokument
+
+            allDvds = (Document) xmlRes.getContentAsDOM();                      // prevede na DOM Node (Document)
+            System.out.println("XML získáno z DB.");
+
+        } catch (XMLDBException ex) {
+            Logger.getLogger(DvdManagerImpl.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (col != null) {
+                try {
+                    col.close();
+                } catch (XMLDBException xe) {
+                    System.err.println("Chyba při získávání dat z DB! " + xe);
+                }
+            }
+            return allDvds;
+        }
     }
 
     @Override
-    public OutputStream getDvdByType(Type type) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public Document getDvdByType(String type) {
+        Document dvd = null;
+        Collection col = null;
+        try {
+            col = DatabaseManager.getCollection(dbURI);
+            XPathQueryService xpqs = (XPathQueryService) col.getService("XPathQueryService", "1.0");
+            xpqs.setProperty("indent", "yes");
+            ResourceSet result = xpqs.query("//dvd[type = '" + type + "']");
+
+            XMLResource xmlRes = (XMLResource) result.getMembersAsResource();
+
+            dvd = (Document) xmlRes.getContentAsDOM();
+
+        } catch (XMLDBException ex) {
+            Logger.getLogger(DvdManagerImpl.class.getName()).log(Level.SEVERE, "Chyba při získávání dvd dle typu!", ex);
+        } finally {
+            if (col != null) {
+                try {
+                    col.close();
+                } catch (XMLDBException xe) {
+                    System.err.println("Chyba při získávání dat z DB! " + xe);
+                }
+            }
+            return dvd;
+        }
     }
 
     @Override
-    public OutputStream getDvdByName(String name) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public Document getDvdByName(String name) {
+        Document dvd = null;
+        Collection col = null;
+        try {
+            col = DatabaseManager.getCollection(dbURI);
+            XPathQueryService xpqs = (XPathQueryService) col.getService("XPathQueryService", "1.0");
+            xpqs.setProperty("indent", "yes");
+            ResourceSet result = xpqs.query("//dvd[name = '" + name + "']");
+            if (result.getSize() > 0) {
+                XMLResource xmlRes = (XMLResource) result.getMembersAsResource();
+
+                dvd = (Document) xmlRes.getContentAsDOM();
+            } else {
+                return null;
+            }
+        } catch (XMLDBException ex) {
+            Logger.getLogger(DvdManagerImpl.class.getName()).log(Level.SEVERE, "Chyba při získávání dvd dle názvu!", ex);
+        } finally {
+            if (col != null) {
+                try {
+                    col.close();
+                } catch (XMLDBException xe) {
+                    System.err.println("Chyba při získávání dat z DB! " + xe);
+                }
+            }
+            return dvd;
+        }
+    }
+
+    @Override
+    public Document getDvdByTitle(String title) {
+        Document dvd = null;
+        Collection col = null;
+        try {
+            col = DatabaseManager.getCollection(dbURI);
+            XPathQueryService xpqs = (XPathQueryService) col.getService("XPathQueryService", "1.0");
+            xpqs.setProperty("indent", "yes");
+            ResourceSet result = xpqs.query("//dvd[titles/title/name = '" + title + "']");
+            if (result.getSize() > 0) {
+                XMLResource xmlRes = (XMLResource) result.getMembersAsResource();
+
+                dvd = (Document) xmlRes.getContentAsDOM();
+            } else {
+                return null;
+            }
+        } catch (XMLDBException ex) {
+            Logger.getLogger(DvdManagerImpl.class.getName()).log(Level.SEVERE, "Chyba při získávání dvd dle titulu!", ex);
+        } finally {
+            if (col != null) {
+                try {
+                    col.close();
+                } catch (XMLDBException xe) {
+                    System.err.println("Chyba při získávání dat z DB! " + xe);
+                }
+            }
+            return dvd;
+        }
+
     }
 }
--- trunk/VideoWeb/src/java/cz/muni/fi/pb138/jaro2011/videoweb/Main.java
+++ trunk/VideoWeb/src/java/cz/muni/fi/pb138/jaro2011/videoweb/Main.java
@@ -6,6 +6,15 @@ package cz.muni.fi.pb138.jaro2011.videoweb;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import org.w3c.dom.Document;
+import org.w3c.dom.DocumentFragment;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 import org.xmldb.api.base.XMLDBException;
 
 /**
@@ -13,12 +22,28 @@ import org.xmldb.api.base.XMLDBException;
  * @author radek
  */
 public class Main {
-    
+
+    private static DvdManagerImpl dm;
+
     public static void main(String[] args) {
         try {
-            DvdManagerImpl dm = new DvdManagerImpl();
+            dm = new DvdManagerImpl();
+
+            //printDocument(dm.getAllDvds());
+
+            //printDocument(dm.getDvdById(2));
+
+            //printDocument(dm.getDvdByType("originál"));
+
+            //printDocument(dm.getDvdByName("Simpsonovi"));
+            
+            //printDocument(dm.getDvdByTitle("Potápění"));
             
             
+
+            System.out.println("Konec programu.");
+
+        
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
@@ -28,5 +53,106 @@ public class Main {
         } catch (XMLDBException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+
+    public static void printDocument(Document doc) {
+
+        if (doc == null) {
+            System.out.println("Prázdný dokument");
+        } else {
+            Element dvds = doc.getDocumentElement();
+            NodeList dvdList = dvds.getElementsByTagName("dvd");
+
+            for (int i = 0; i < dvdList.getLength(); i++) {        // prochazi dvd
+                Element dvd = (Element) dvdList.item(i);         // vezme i-te dvd
+
+                Element name = (Element) dvd.getElementsByTagName("name").item(0);
+                System.out.println("Název dvd: " + name.getTextContent());
+
+                Long id = Long.parseLong(dvd.getAttribute("id"));
+                System.out.println("ID: " + id);
+
+                Element type = (Element) dvd.getElementsByTagName("type").item(0);
+                System.out.println("Typ dvd: " + type.getTextContent());
+
+                Element titles = (Element) dvd.getElementsByTagName("titles").item(0);
+                NodeList titleList = titles.getElementsByTagName("title");
+
+                for (int j = 0; j < titleList.getLength(); j++) {
+                    Element title = (Element) titleList.item(j);
+
+                    Element titleName = (Element) title.getElementsByTagName("name").item(0);
+                    System.out.println("Titul: " + titleName.getTextContent());
+
+                    Element titleRepresentative = (Element) title.getElementsByTagName("representative").item(0);
+                    System.out.println("Hl. představitel: " + titleName.getTextContent());
+
+                }
+
+            }
+        }
+    }
+
+    private static void addDvd() throws ParserConfigurationException, TransformerConfigurationException, TransformerException {
+        
+        //We need a Document
+            DocumentBuilderFactory dbfac = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = dbfac.newDocumentBuilder();
+            Document doc  = docBuilder.newDocument();
+            
+            DocumentFragment df = doc.createDocumentFragment();
+
+            ////////////////////////
+            //Creating the XML tree
+
+            //create the root element and add it to the document
+            Element dvdLib = doc.createElement("dvd-library");
+            df.appendChild(dvdLib);
+            
+            //create the root element and add it to the document
+            Element dvd = doc.createElement("dvd");
+            dvd.setAttribute("id", "111");
+            dvdLib.appendChild(dvd);
+
+            //create child element, add an attribute, and add to root
+            Element name = doc.createElement("name");
+            name.setTextContent("Top Gear");
+            dvd.appendChild(name);
+            
+            Element type = doc.createElement("type");
+            type.setTextContent("magazín");
+            dvd.appendChild(type);
+
+            Element titles = doc.createElement("titles");
+            dvd.appendChild(titles);
+            
+            //first title
+            Element title = doc.createElement("title");
+            titles.appendChild(title);
+            
+            Element titleName =  doc.createElement("name");
+            titleName.setTextContent("Top Gear Uncovered");
+            title.appendChild(titleName);
+            
+            Element titleRep =  doc.createElement("representative");
+            titleRep.setTextContent("Richard Hammond");
+            title.appendChild(titleRep);
+            
+            // second title
+            Element title1 = doc.createElement("title");
+            titles.appendChild(title1);
+            
+            Element titleName1 =  doc.createElement("name");
+            titleName1.setTextContent("The Italian Job");
+            title1.appendChild(titleName1);
+            
+            Element titleRep1 =  doc.createElement("representative");
+            titleRep1.setTextContent("Jeremy Clarkson");
+            title1.appendChild(titleRep1);
+            
+            
+            
+            //printDocument(dm.createDvd(df));
+            
     }
 }
