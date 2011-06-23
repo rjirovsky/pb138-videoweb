a8ddf2e93871844f25d0927da48512b3bf10346a
rjirovsky@gmail.com <rjirovsky@gmail.com@c4821f4a-e21e-2dad-b0b3-6af572229c24>
2011-06-23 14:29:10 +0000

    File

--- VideoWeb/src/java/cz/muni/fi/pb138/jaro2011/videoweb/VideoWebManager.java
+++ VideoWeb/src/java/cz/muni/fi/pb138/jaro2011/videoweb/VideoWebManager.java
@@ -22,6 +22,6 @@ public interface VideoWebManager {
     
     public Node getAllDvds();
     
-    public void importDvdsFromODF(String name);
+    public void importDvdsFromODF(File file);
     
 }
